c19587b0c77b6f09f1caa6f6c01e8d539633bb53
shan-akbar <shanakbarsanga@gmail.com>
2025-06-03 17:29:24 +0500

    ypp

---
+++ assets/custom-pages.js
@@ -0,0 +1,43 @@
+$('.diary-flip_next-btn').click(function(){
+        let visible_pages = Number($(".diary-flip_page.last__page").data("pageindex"));
+
+        let flipPage = $(this).closest('.diary-flip_page');
+        let current_index = Number(flipPage.data("pageindex"));
+    if(current_index <= visible_pages){
+        flipPage.addClass('flipped');
+        let current_zindex = flipPage.css('zIndex');
+        flipPage.css('zIndex', 3000-Number(current_zindex));
+        let page_index = Number(flipPage.data('pageindex'));
+        let prev_index = page_index - 2;
+        let next_index = page_index + 2;
+        if(prev_index > 0){
+            let prev_page = $(`.diary-flip_page__${prev_index}`);
+            prev_page.addClass('display__none');
+        }
+        if(next_index < 3000){
+            let next_page = $(`.diary-flip_page__${next_index}`);
+            if(!(flipPage.hasClass("second_last__page") || flipPage.hasClass("last__page"))){
+                next_page.removeClass('display__none');
+            }
+        }
+    }
+
+});
+
+$('.diary-flip_back-btn').click(function(){
+    let flipPage = $(this).closest('.diary-flip_page');
+    flipPage.removeClass('flipped');
+    let current_zindex = flipPage.css('zIndex');
+    flipPage.css('zIndex', 3000-Number(current_zindex));
+    let page_index = Number(flipPage.data('pageindex'));
+    let prev_index = page_index - 2;
+    let next_index = page_index + 2;
+    if(prev_index > 0){
+        let prev_page = $(`.diary-flip_page__${prev_index}`);
+        prev_page.removeClass('display__none');
+    }
+    if(next_index < 3000){
+        let next_page = $(`.diary-flip_page__${next_index}`);
+        next_page.addClass('display__none');
+    }
+});
